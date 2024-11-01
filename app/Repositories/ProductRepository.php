0680a99cbdf5a3414bfa7ea8b841ad9d16e2b47a
kiipod <iamkir90@hotmail.com>
2024-11-01 21:21:22 +0300

    Добавляет метод для вывода продуктов

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -2,9 +2,44 @@
 
 declare(strict_types=1);
 
-namespace Repositories;
+namespace Kiipod\ShopTelegramBot\Repositories;
 
-interface ProductRepository
+use Kiipod\ShopTelegramBot\Database\MysqlService;
+use PDO;
+use PDOException;
+
+class ProductRepository implements ProductRepositories
 {
-    //
+    private MysqlService $db;
+
+    public function __construct()
+    {
+        $this->db = new MysqlService();
+        $this->db->connect('mysql', 'root', 'password', 'shop');
+    }
+
+    /**
+     * Получить список продуктов из базы данных
+     *
+     * @return array|null Массив продуктов или null в случае ошибки
+     */
+    public function getProducts(): ?array
+    {
+        // Получаем соединение PDO
+        $pdo = $this->db->getConnection();
+
+        if ($pdo) {
+            try {
+                $stmt = $pdo->query('SELECT * FROM products');
+                return $stmt->fetchAll(PDO::FETCH_ASSOC);
+            } catch (PDOException $e) {
+                // Обработка ошибки запроса
+                echo "Ошибка выполнения запроса: " . $e->getMessage();
+                return null;
+            }
+        }
+
+        // Возвращаем null, если соединение не установлено
+        return null;
+    }
 }
