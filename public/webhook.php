8a421b670e6897eea23bcd6a523ea7c307e3235e
kiipod <iamkir90@hotmail.com>
2024-11-07 21:24:18 +0300

    Добавляет helper для чтения env-файла

--- public/webhook.php
+++ public/webhook.php
@@ -2,9 +2,18 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use Kiipod\ShopTelegramBot\Helpers\EnvHelper;
 use Kiipod\ShopTelegramBot\Telegram\Webhook;
 
-$botToken = '8160278396:AAEhWW3AMxHvilo6XAouWSUee9GA0dnGm9o';
+$envHelper = new EnvHelper();
+
+try {
+    $env = $envHelper->readEnv('../.env');
+} catch (Exception $e) {
+    echo "Ошибка при чтении .env файла: " . $e->getMessage();
+}
+
+$botToken = $env['BOT_TOKEN'];
 $webhookUrl = 'https://kiipod.ru/webhook.php';
 $telegramWebhook = new Webhook($botToken);
 
